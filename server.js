cbf4306d52f10e4ebb869251ecffbac4500dc3ec
강현구 <rkdgusrnrlrl@gmail.com>
2017-11-18 15:58:25 +0900

    웹소켓 mqtt 서버 이벤트 핸들리 추가

--- server.js
+++ server.js
@@ -14,12 +14,51 @@ if (handler) {
 
 wss.on('connection', function (ws) {
     var stream = websocket(ws)
+
+    // 타임 아웃 설절 5분
+    stream.setTimeout(1000 * 60 * 5)
+
     var connection = new Connection(stream)
 
-    handle(connection)
+    handle(connection);
+
+    stream.on('timeout', function () {
+        connection.destroy()
+    })
 })
 
-function handle (conn) {
-    // handle the MQTT connection like
-    // the net example
+function handle (client) {
+
+    // client connected
+    client.on('connect', function (packet) {
+        // acknowledge the connect packet
+        client.connack({ returnCode: 0 })
+    })
+
+    // client published
+    client.on('publish', function (packet) {
+        // send a puback with messageId (for QoS > 0)
+        client.puback({ messageId: packet.messageId })
+    })
+
+    // client pinged
+    client.on('pingreq', function () {
+        // send a pingresp
+        client.pingresp()
+    });
+
+    // client subscribed
+    client.on('subscribe', function (packet) {
+        // send a suback with messageId and granted QoS level
+        client.suback({ granted: [packet.qos], messageId: packet.messageId })
+    })
+
+    // timeout idle streams after 5 minutes
+    stream.setTimeout(1000 * 60 * 5)
+
+    // connection error handling
+    client.on('close', function () { client.destroy() })
+    client.on('error', function () { client.destroy() })
+    client.on('disconnect', function () { client.destroy() })
+
 }
