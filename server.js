0553b3b8c137adb33518bdbfc6afbcafa8c8f9f8
강현구 <rkdgusrnrlrl@gmail.com>
2017-11-22 21:19:20 +0900

    홀따음표 쌍따음표로 변경

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /**
  * Created by rkdgusrnrlrl on 17. 11. 18.
  */
-var websocket = require('websocket-stream')
-var WebSocketServer = require('ws').Server
-var Connection = require('mqtt-connection')
-var http = require('http');
+var websocket = require("websocket-stream")
+var WebSocketServer = require("ws").Server
+var Connection = require("mqtt-connection")
+var http = require("http");
 var server = http.createServer(function (request, response) {
 
     let jsonData = "";
@@ -25,8 +25,7 @@ var wss = new WebSocketServer({server: server})
 
 var topicMap = {};
 
-
-wss.on('connection', function (ws) {
+wss.on("connection", function (ws) {
     var stream = websocket(ws)
     var connection = new Connection(stream)
 
@@ -36,45 +35,45 @@ wss.on('connection', function (ws) {
 function handle (client) {
 
     // client connectedCreate a client instance
-    client.on('connect', function (packet) {
+    client.on("connect", function (packet) {
         // acknowledge the connect packet
 
         client.connack({ returnCode: 0 })
-        let topicPool = topicMap['hello']
+        let topicPool = topicMap["hello"]
         if(topicPool) {
             topicPool.push(client)
         } else {
-            topicMap['hello'] = [client];
+            topicMap["hello"] = [client];
         }
     })
 
     // client published
-    client.on('publish', function (packet) {
+    client.on("publish", function (packet) {
         // send a puback with messageId (for QoS > 0)
         client.puback({ messageId: packet.messageId })
     })
 
     // client pinged
-    client.on('pingreq', function () {
+    client.on("pingreq", function () {
         // send a pingresp
         client.pingresp()
     });
 
     // client subscribed
-    client.on('subscribe', function (packet) {
+    client.on("subscribe", function (packet) {
         // send a suback with messageId and granted QoS level
         client.suback({ granted: [packet.qos], messageId: packet.messageId })
     })
 
     // connection error handling
-    client.on('close', function () {
+    client.on("close", function () {
         console.log("close")
         client.destroy()
     })
 
-    client.on('error', function () { client.destroy() })
-    client.on('disconnect', function () {
-        console.log('disconnect')
+    client.on("error", function () { client.destroy() })
+    client.on("disconnect", function () {
+        console.log("disconnect")
         client.destroy()
     })
 
@@ -90,5 +89,5 @@ function sentHello(msg) {
 
 
 server.listen(8000, function () {
-    console.log('Listening on %d', server.address().port);
+    console.log("Listening on %d", server.address().port);
 })
